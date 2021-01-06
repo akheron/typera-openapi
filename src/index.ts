4121431c1c291294ff8ce2b72e7aedcda4336141
Petri Lehtinen <petri@digip.org>
2021-01-06 22:04:21 +0200

    Add prefix() function
    It can be used to prefix OpenAPI paths.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
 export { generate } from './generate'
+import { OpenAPIV3 } from 'openapi-types'
+
+export const prefix = (
+  prefix: string,
+  paths: OpenAPIV3.PathsObject
+): OpenAPIV3.PathsObject =>
+  Object.fromEntries(
+    Object.entries(paths).map(([path, value]) => [prefix + path, value])
+  )
