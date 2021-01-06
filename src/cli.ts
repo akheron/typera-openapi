f96aa006ac29632dd0c19139445d330b2fbe2dad
Petri Lehtinen <petri@digip.org>
2021-01-06 21:06:12 +0200

    Write files

--- src/cli.ts
+++ src/cli.ts
@@ -1,15 +1,51 @@
+import * as fs from 'fs'
+import * as path from 'path'
 import { generate } from '.'
 import { Logger } from './context'
+import * as yargs from 'yargs'
 
 const log: Logger = (location, level, ...args) =>
   console.log(`${location}: ${level}:`, ...args)
 
-function main() {
-  const args = process.argv.slice(2)
-  const result = generate(args, { strict: true }, { log })
-  result.forEach(({ fileName, paths }) => {
-    console.log(fileName)
-    console.log(JSON.stringify(paths, null, 2))
+type Format = 'ts' | 'json'
+
+const parseArgs = () =>
+  yargs.option('format', {
+    description: 'Output file format',
+    choices: ['ts' as const, 'json' as const],
+    default: 'ts' as Format,
+  }).argv
+
+const outputFileName = (sourceFileName: string, ext: string): string =>
+  sourceFileName.slice(0, -path.extname(sourceFileName).length) + ext
+
+const main = () => {
+  const args = parseArgs()
+
+  const sourceFiles = args._
+  const ext = args.format === 'ts' ? '.openapi.ts' : '.json'
+
+  const results = generate(sourceFiles, { strict: true }, { log }).map(
+    result => ({
+      ...result,
+      outputFileName: outputFileName(result.fileName, ext),
+    })
+  )
+
+  results.forEach(({ outputFileName, paths }) => {
+    const resultObject = JSON.stringify({ paths })
+    const content =
+      args.format === 'ts'
+        ? `\
+import { OpenAPIV3 } from 'openapi-types'
+
+const spec: { paths: OpenAPIV3.PathsObject } = ${resultObject};
+
+export default spec;
+`
+        : resultObject
+    console.log('Writing', outputFileName)
+    fs.writeFileSync(outputFileName, content)
   })
 }
 
