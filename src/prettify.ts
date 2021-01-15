1225c9f8236efacac8c58391184abff0f329aa83
Petri Lehtinen <petri@digip.org>
2021-01-15 21:54:07 +0200

    Add --prettify CLI option

---
+++ src/prettify.ts
@@ -0,0 +1,18 @@
+export async function runPrettier(
+  filePath: string,
+  tsCode: string
+): Promise<string> {
+  let format, resolveConfig
+  try {
+    // eslint-disable-next-line @typescript-eslint/no-extra-semi
+    ;({ format, resolveConfig } = require('prettier'))
+  } catch (_err) {
+    console.warn(
+      'WARNING: prettier is not installed, unable to prettify output'
+    )
+    return tsCode
+  }
+
+  const options = await resolveConfig(filePath)
+  return await format(tsCode, { ...options, filepath: filePath })
+}
