e637e602c6680bde0ec7c149d0bef66adae2acc3
Petri Lehtinen <petri@digip.org>
2021-01-15 21:54:07 +0200

    Add --prettify CLI option

--- src/cli.ts
+++ src/cli.ts
@@ -3,7 +3,9 @@ import * as fs from 'fs'
 import * as path from 'path'
 import { generate } from '.'
 import { Logger } from './context'
+import { runPrettier } from './prettify'
 import * as yargs from 'yargs'
+import { OpenAPIV3 } from 'openapi-types'
 
 const log: Logger = (location, level, ...args) =>
   console.log(`${location}: ${level}:`, ...args)
@@ -11,16 +13,24 @@ const log: Logger = (location, level, ...args) =>
 type Format = 'ts' | 'json'
 
 const parseArgs = () =>
-  yargs.usage('Usage: $0 [options] FILE...').option('format', {
-    description: 'Output file format',
-    choices: ['ts' as const, 'json' as const],
-    default: 'ts' as Format,
-  }).argv
+  yargs
+    .usage('Usage: $0 [options] FILE...')
+    .option('format', {
+      description: 'Output file format',
+      choices: ['ts' as const, 'json' as const],
+      default: 'ts' as Format,
+    })
+    .option('prettify', {
+      alias: 'p',
+      description: 'Apply prettier to output TypeScript files',
+      type: 'boolean',
+      default: false,
+    }).argv
 
 const outputFileName = (sourceFileName: string, ext: string): string =>
   sourceFileName.slice(0, -path.extname(sourceFileName).length) + ext
 
-const main = () => {
+const main = async () => {
   const args = parseArgs()
 
   const sourceFiles = args._.map((x) => x.toString())
@@ -33,21 +43,25 @@ const main = () => {
     })
   )
 
-  results.forEach(({ outputFileName, paths }) => {
-    const resultObject = JSON.stringify({ paths })
-    const content =
-      args.format === 'ts'
-        ? `\
+  for (const { outputFileName, paths } of results) {
+    let content = args.format === 'ts' ? tsString(paths) : jsonString(paths)
+    if (args.prettify) {
+      content = await runPrettier(outputFileName, content)
+    }
+    console.log('Writing', outputFileName)
+    fs.writeFileSync(outputFileName, content)
+  }
+}
+
+const tsString = (paths: OpenAPIV3.PathsObject): string => `\
 import { OpenAPIV3 } from 'openapi-types'
 
-const spec: { paths: OpenAPIV3.PathsObject } = ${resultObject};
+const spec: { paths: OpenAPIV3.PathsObject } = ${JSON.stringify({ paths })};
 
 export default spec;
 `
-        : resultObject
-    console.log('Writing', outputFileName)
-    fs.writeFileSync(outputFileName, content)
-  })
-}
+
+const jsonString = (paths: OpenAPIV3.PathsObject): string =>
+  JSON.stringify({ paths })
 
 main()
