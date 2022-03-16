f5a3ea7f7cbea92c7b856655d21ad0d8f3057e8a
Petri Lehtinen <petri@digip.org>
2022-03-16 22:12:03 +0200

    Reformat

--- tests/other-routes.ts
+++ tests/other-routes.ts
@@ -1,6 +1,8 @@
 import { Response, route, Route, router } from 'typera-express'
 
-const otherRoute: Route<Response.Ok<string>> = route.get('/other-route').handler(() => Response.ok('hello'))
+const otherRoute: Route<Response.Ok<string>> = route
+  .get('/other-route')
+  .handler(() => Response.ok('hello'))
 
 /** @prefix /other-stuff */
 export default router(otherRoute)
