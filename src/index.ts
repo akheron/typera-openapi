b9bcc86a90a9f023d0254f94aaf116e3f22d8b6d
Petri Lehtinen <petri@digip.org>
2022-03-18 20:34:50 +0200

    Remove the now unneeded prefix() function

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,2 @@
 export { GenerateResult, generate } from './generate'
 export { LogLevel } from './context'
-import { OpenAPIV3 } from 'openapi-types'
-
-export const prefix = (
-  prefix: string,
-  paths: OpenAPIV3.PathsObject
-): OpenAPIV3.PathsObject =>
-  Object.fromEntries(
-    Object.entries(paths).map(([path, value]) => [prefix + path, value])
-  )
