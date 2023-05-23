a6f578efca3115df594e61ecc660d3348ce52439
Vilppu Saarinen <vilppu.saarinen@reaktor.com>
2023-05-23 10:28:29 +0300

    Support documenting route params
    Implemented routeParam descriptions and validation

--- tests/other-routes.ts
+++ tests/other-routes.ts
@@ -1,5 +1,14 @@
 import { Response, route, Route, router } from 'typera-express'
 
+/**
+ * @routeParam myRouteParameter 12345
+ */
+const routeWithRouteParameterTag: Route<
+  Response.Ok<string> | Response.BadRequest<string, undefined>
+> = route
+  .get('/route-with-route-parameter-tag/:myRouteParameter')
+  .handler(() => Response.ok('hello'))
+
 const otherRoute: Route<Response.Ok<string>> = route
   .get('/other-route')
   .handler(() => Response.ok('hello'))
@@ -16,4 +25,4 @@ const routeWithTag: Route<Response.Ok<string>> = route
  *  @prefix /other-stuff
  *  @tags TagFromRouter
  */
-export default router(otherRoute, routeWithTag)
+export default router(otherRoute, routeWithTag, routeWithRouteParameterTag)
